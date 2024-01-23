596af91c7e9120e119a93d3a186aa39052344851
Chi-G <chijindu.nwokeohuru@gmail.com>
2024-01-23 02:44:06 -0800

    updates on 3 quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -88,7 +88,7 @@ void sorter(int **array, size_t lb, size_t ub, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
 	sorter(&array, 0, size - 1, size);
-} e
+}
